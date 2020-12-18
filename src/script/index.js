422a84ea2791b545686ee9eeb8c9734e7e3bcbd2
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-12-17 22:35:42 -0300

    Enhancing index.js code and the media queries

--- src/script/index.js
+++ src/script/index.js
@@ -1,35 +1,78 @@
 function getCountdownType(minutes) {
-  const btns = [
-    window.document.querySelector("#time-pomodoro"),
-    window.document.querySelector("#time-pause"),
-    window.document.querySelector("#time-big-pause"),
-  ];
+  const timerText = getElement("timer");
+  const pageTitle = getElement("page-title");
   const countdown = new Date(new Date().getTime() + minutes * 60000);
-  const timerText = window.document.getElementsByClassName("timer")[0];
-  const pageTitle = window.document.getElementsByClassName("page-title")[0];
+
+  workCountdown([countdown, timerText, pageTitle]);
+}
+
+function workCountdown([countdown, timerText, pageTitle]) {
+  getTimerButtons();
 
   const actCountdown = setInterval(function () {
+    // Calc timer
     const now = new Date().getTime();
-
     const distance = countdown - now;
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    timerText.innerText = ` ${minutes}:${seconds} `;
-    pageTitle.innerText = ` ${minutes}:${seconds} PomodoroTimer`;
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+    {
+      // Parsing something like 5:0 to 05:00
+      var x = secs.toString();
+      var y = mins.toString();
+
+      x.length === 1 ? (x = [...(x += 0)].reverse().join("")) : null;
+      y.length === 1 ? (y = [...(y += 0)].reverse().join("")) : null;
+    }
+
+    // Printing the timer
+    timerText.innerText = ` ${y}:${x} `;
+    pageTitle.innerText = ` ${y}:${x} PomodoroTimer`;
+
+    // Prevent timers at the same time
     btns.map((item) =>
       item.addEventListener("click", () => clearInterval(actCountdown))
     );
-    console.log(typeof seconds)
+
+    // When timer finishes
     if (distance <= 0) {
       clearInterval(actCountdown);
       timerText.innerHTML = '<i class="fas fa-check"></i>';
-      pageTitle.innerText = `PomodoroTimer`;
-
-      playAudio();
+      pageTitle.innerText = `00:00 ` + getPhrase();
+      playFinishAlarm();
     }
   }, 1000);
 }
 
-function playAudio() {
+const getTimerButtons = () => {
+  return (btns = [
+    window["time-big-pause"],
+    window["time-pause"],
+    window["time-pomodoro"],
+  ]);
+};
+
+function getElement(element) {
+  return window.document.querySelector(`.${element}`);
+}
+
+function playFinishAlarm() {
   window.document.querySelector("audio").play();
 }
+
+function getPhrase() {
+  const phrases = [
+    "DONE ✔",
+    "Way to go!",
+    "Congrats!",
+    "FINISH!",
+    "Play it again...",
+    "You rock!",
+  ];
+  const sort = Math.floor(Math.random() * phrases.length);
+  return phrases[sort];
+}
+
+(function () {
+  console.log("Hello, world");
+})();
