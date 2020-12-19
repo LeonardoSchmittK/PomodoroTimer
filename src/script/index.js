29704d1cd0e53c99b4123b1c6f11adf470bbd99e
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-12-18 22:23:14 -0300

    Arranging scss code

--- src/script/index.js
+++ src/script/index.js
@@ -1,17 +1,14 @@
 app = (function () {
-  const btns = [
-    window["time-big-pause"],
-    window["time-pause"],
-    window["time-pomodoro"],
+  const [btns, timers, timerText, pageTitle] = [
+    [window["time-big-pause"], window["time-pause"], window["time-pomodoro"]],
+    [10.01, 5.01, 25.01],
+    document.querySelector(".timer"),
+    document.querySelector(".page-title"),
   ];
-  const timers = [10.01, 5.01, 25.01];
 
   btns.map((btn, i) =>
     btn.addEventListener("click", () => getCountdownType(timers[i]))
   );
-
-  const timerText = document.querySelector(".timer");
-  const pageTitle = document.querySelector(".page-title");
 
   function getCountdownType(minutes) {
     const countdown = new Date(new Date().getTime() + minutes * 60000);
@@ -21,7 +18,7 @@ app = (function () {
 
   function executeCountdown([countdown, timerText, pageTitle]) {
     const actCountdown = setInterval(function () {
-      // Calc timer
+      // Calculate timer
       const now = new Date().getTime();
       const distance = countdown - now;
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -29,8 +26,7 @@ app = (function () {
 
       {
         // Parsing something like 5:0 to 05:00
-        var secsStr = seconds.toString();
-        var minsStr = minutes.toString();
+        var [secsStr, minsStr] = [seconds.toString(), minutes.toString()];
 
         minsStr.length === 1
           ? (minsStr = [...(minsStr += 0)].reverse().join(""))
@@ -42,15 +38,14 @@ app = (function () {
 
       // Printing the timer
       timerText.innerText = ` ${minsStr}:${secsStr} `;
-      pageTitle.innerText = ` ${minsStr}:${secsStr} PomodoroTimer`;
+      pageTitle.innerText = ` ${minsStr}:${secsStr} PomodoroTimer `;
+      timerText.style.animation = " timer-decrease 1s infinite";
 
       // Prevent timers at the same time
       btns.map((btn) =>
         btn.addEventListener("click", () => clearInterval(actCountdown))
       );
 
-      timerText.style.animation = " timer-decrease 1s infinite";
-      
       // When timer finishes
       if (distance <= 0) {
         clearInterval(actCountdown);
@@ -60,6 +55,7 @@ app = (function () {
       }
     }, 1000);
   }
+
   function playFinishAlarm() {
     document.querySelector("audio").play();
   }
